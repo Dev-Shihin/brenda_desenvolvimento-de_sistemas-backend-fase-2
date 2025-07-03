52f9c2660b468e31e2140de7af76f17f8c806cf3
Dev <138815417+Dev-Shihin@users.noreply.github.com>
2025-07-03 20:52:27 -0300

    Delete servico-planos-ativos directory

--- servico-planos-ativos/src/dominio/interfaces/plano-ativo.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { PlanoAtivo } from '../entities/plano-ativo.entity';
-
-export interface IPlanoAtivoRepository {
-  create(planoAtivo: PlanoAtivo): Promise<PlanoAtivo>;
-  findByClienteId(clienteId: number): Promise<PlanoAtivo[]>;
-  findOne(options: any): Promise<PlanoAtivo | null>;
-  save(planoAtivo: PlanoAtivo): Promise<PlanoAtivo>;
-}
